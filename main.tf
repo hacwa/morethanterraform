65f4eaa758674348dbd08b0037458ae838af98a0
hac wa <admin@hacwa.co.uk>
2023-02-10 16:14:41 +0000

    count

--- main.tf
+++ main.tf
@@ -16,44 +16,18 @@ resource "docker_image" "nodered_image" {
 }
 
 resource "random_string" "random" {
+  count = 2
   length = 4
   special = false
   upper = false
 }
 
-resource "random_string" "random2" {
-  length = 4
-  special = false
-  upper = false
-}
-
-resource "docker_container" "nodered_container1" {
-  name  = join("-",["nodered", random_string.random.result])
+resource "docker_container" "nodered_container0" {
+  name  = join("-",["nodered", random_string.random[count.index].result])
+  count = 2 
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
 #    external = 1880
   }
-}
-
-resource "docker_container" "nodered_container2" {
-  name  = join("-",["nodered", random_string.random2.result])
-  image = docker_image.nodered_image.latest
-  ports {
-    internal = 1880
-#    external = 1880
-  }
-}
-
-
-output "container1" {
-  value = join(":", [docker_container.nodered_container1.ip_address, docker_container.nodered_container1.ports[0].external, docker_container.nodered_container1.name] )
-  description = "the name, IP address and external port of the container"
-}
-output "container2" {
-  value = join(":", [docker_container.nodered_container2.ip_address, docker_container.nodered_container2.ports[0].external, docker_container.nodered_container2.name] )
-  description = "the name, IP address and external port of the container"
-}
-output "container_names"{
-  value = join("____and_den____", [docker_container.nodered_container1.name ,docker_container.nodered_container2.name ])
 }
