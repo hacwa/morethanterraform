1423e1f364d742bd9c24f9e7bd7ad43707d1c5b5
hac wa <admin@hacwa.co.uk>
2023-02-10 16:20:50 +0000

    rename

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "random_string" "random" {
   upper = false
 }
 
-resource "docker_container" "nodered_container0" {
+resource "docker_container" "nodered_container" {
   name  = join("-",["nodered", random_string.random[count.index].result])
   count = 2 
   image = docker_image.nodered_image.latest
@@ -30,4 +30,13 @@ resource "docker_container" "nodered_container0" {
     internal = 1880
 #    external = 1880
   }
+}
+
+output "container1" {
+  value = join(":", [docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].ports[0].external, docker_container.nodered_container[0].name] )
+  description = "the name, IP address and external port of the container"
+}
+output "container2" {
+  value = join(":", [docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].ports[0].external, docker_container.nodered_container[1].name] )
+  description = "the name, IP address and external port of the container"
 }
